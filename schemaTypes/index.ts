9cfa731c0eee6ef597d6e4530bbae0ac2ddc5b38
Eben <kwabenat679@gmail.com>
2024-11-01 08:13:06 +0000

    updated hotelRoom schema and added review schema

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -1,5 +1,7 @@
 import user from "./user";
 import account from "./account";
 import booking from "./booking";
+import hotelRoom from "./hotelRoom";
+import review from "./review";
 
-export const schemaTypes = [user, account, booking];
+export const schemaTypes = [user, account, booking, hotelRoom, review];
