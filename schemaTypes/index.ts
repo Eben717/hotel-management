bbf678a1d67740e91ebf57c03e748b28e10aebbb
Eben <kwabenat679@gmail.com>
2024-11-03 21:10:44 +0000

    added verificationToken

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -3,5 +3,6 @@ import account from "./account";
 import booking from "./booking";
 import hotelRoom from "./hotelRoom";
 import review from "./review";
+import verificationToken from "./verificationToken";
 
-export const schemaTypes = [user, account, booking, hotelRoom, review];
+export const schemaTypes = [user, account, booking, hotelRoom, review, verificationToken];
